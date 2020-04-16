58d3c6be69824b4da52d1258e548a37b7c3e65e6
idanless <31201417+idanless@users.noreply.github.com>
2020-04-16 12:06:28 +0300

    Update cliptotype.py

--- cliptotype.py
+++ cliptotype.py
@@ -2,12 +2,24 @@ import keyboard
 import pyperclip
 import time
 
+class Key:
+    def __init__(self,clip="test"):
+        self.keywait = keyboard.wait('left alt+left ctrl+a')
+        self.clip = clip
+        self.write = keyboard.write(self.copyclip())
+
+    def copyclip(self):
+        clipboard_content = pyperclip.paste()
+        clipboard_content.split()
+        print("write this ", clipboard_content)
+        return clipboard_content
+
+    def getclip(self):
+        return self.copyclip()
+
 
 
 while True:
-    time.sleep(1)
-    keyboard.wait('left alt+left ctrl+a')
-    clipboard_content = pyperclip.paste()
-    clipboard_content.split()
-    print("write this ",clipboard_content)
-    keyboard.write(clipboard_content)
+    key = Key()
+    key.keywait
+    key.write
